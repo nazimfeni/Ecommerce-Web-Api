021266687a0828386309874243633b4e99e0b7aa
nazimfeni <mdnazimuddin77@gmail.com>
2026-07-15 00:16:34 +0600

    updated Get Rest API

--- Program.cs
+++ Program.cs
@@ -4,5 +4,32 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () => 
+{
+    return "API is working";
+
+});
+
+app.MapGet("/hello", () => 
+{
+    return "Hello Zarif";
+
+});
+
+app.MapPost("/hello", () => 
+{
+    return "Post Method Called";
+
+});
+app.MapPut("/hello", () => 
+{
+    return "Put Method Called";
+
+});
+app.MapDelete("/hello", () => 
+{
+    return "Delete Method Called";
+
+});
 
 app.Run();
